4650b3e9eb16777a02be6e6232e305c0df98e652
HugoRodriguez2022 <119142545+HugoRodriguez2022@users.noreply.github.com>
2024-09-03 20:03:00 -0500

    Archivo utilidades_texto.php agregado correctamente

--- PARCIALES/PARCIAL_1/utilidades_texto.php
+++ PARCIALES/PARCIAL_1/utilidades_texto.php
@@ -1,3 +1,23 @@
 <?php
-    
+function contar_palabras($texto) {
+    return str_word_count($texto);
+}
+
+function contar_vocales($texto) {
+    $vocales = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    $contador = 0;
+    for ($i = 0; $i < strlen($texto); $i++) {
+        if (in_array($texto[$i], $vocales)) {
+            $contador++;
+        }
+    }
+    return $contador;
+}
+
+function invertir_palabras($texto) {
+    $palabras = explode(' ', $texto);
+    $palabras_invertidas = array_reverse($palabras);
+    return implode(' ', $palabras_invertidas);
+}
 ?>
+
