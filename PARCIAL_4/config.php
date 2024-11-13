c3fc98caa3d5bffff08b1a63ea976bce0a03cf4c
HugoRodriguez2022 <119142545+HugoRodriguez2022@users.noreply.github.com>
2024-11-12 20:31:51 -0500

    Delete PARCIAL_4 directory

--- PARCIAL_4/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('GOOGLE_CLIENT_ID', '215269437339-jjb2skno1s5v7q6nklnkohpo614po72t.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URL', 'http://localhost/PARCIALES/PARCIAL_4/callback.php');
-define('GOOGLE_AUTH_URL', 'https://accounts.google.com/o/oauth2/auth');
-define('GOOGLE_TOKEN_URL', 'https://oauth2.googleapis.com/token');
-define('GOOGLE_USER_INFO_URL', 'https://www.googleapis.com/oauth2/v2/userinfo');
-?>
